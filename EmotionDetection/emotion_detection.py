e62dcf1ba2100c361b7aba405a0cc7e7865fd1ba
chinmay-c-326 <chinmaychopade206@gmail.com>
2026-08-17 13:03:12 -0400

    Final commit completed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -17,6 +17,16 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, headers=headers, json=input_json)
 
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
     response_data = json.loads(response.text)
 
     emotions = response_data["emotionPredictions"][0]["emotion"]
